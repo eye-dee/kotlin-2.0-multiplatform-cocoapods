8324f8344a4b41d22eeec437fd1d08c388312e31
eye-dee <drozdovigor1996@gmail.com>
2024-05-30 08:14:35 +0200

    Make sure GoogleSingIn is reachable

--- composeApp/src/iosMain/kotlin/GoogleAuthProviderImpl.kt
+++ composeApp/src/iosMain/kotlin/GoogleAuthProviderImpl.kt
@@ -1,10 +1,8 @@
-import androidx.compose.runtime.Composable
 import cocoapods.GoogleSignIn.GIDSignIn
 import kotlinx.cinterop.ExperimentalForeignApi
 
 internal class GoogleAuthProviderImpl {
 
-    @Composable
     fun getUiProvider() = GoogleAuthUiProviderImpl()
 
     @OptIn(ExperimentalForeignApi::class)
--- composeApp/src/iosMain/kotlin/Platform.ios.kt
+++ composeApp/src/iosMain/kotlin/Platform.ios.kt
@@ -1,7 +1,22 @@
+import kotlinx.coroutines.runBlocking
 import platform.UIKit.UIDevice
 
-class IOSPlatform: Platform {
-    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
+class IOSPlatform : Platform {
+    override val name: String =
+        UIDevice.currentDevice.systemName() +
+                " " +
+                UIDevice.currentDevice.systemVersion +
+                GoogleAuthProviderImpl()
+                    .getUiProvider()
+                    .hashCode()
 }
 
-actual fun getPlatform(): Platform = IOSPlatform()
+actual fun getPlatform(): Platform =
+    IOSPlatform()
+        .also {
+            runBlocking {
+                GoogleAuthProviderImpl()
+                    .getUiProvider()
+                    .signIn()
+            }
+        }
